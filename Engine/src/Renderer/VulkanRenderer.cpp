9e598469baa7b5a65a5cb4ad3a8d6fe7f359cb17
Newt <guoching130@gmail.com>
2022-08-09 03:43:57 +0800

    a

--- Engine/src/Renderer/VulkanRenderer.cpp
+++ Engine/src/Renderer/VulkanRenderer.cpp
@@ -52,7 +52,7 @@ namespace Wizard {
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "Winzard Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.apiVersion = VK_API_VERSION_1_3;
 
         VkInstanceCreateInfo instanceCreateInfo {};
         instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
